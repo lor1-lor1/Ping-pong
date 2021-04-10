dc925cfafbfa928f6f457c353e2684ea53ca2bcf
lor1-lor1 <81848154+lor1-lor1@users.noreply.github.com>
2021-04-10 14:27:56 +0700

    Ping-pong
     Ping-Pong V 1.0

--- ping-pong.py
+++ ping-pong.py
@@ -2,6 +2,10 @@ from pygame import *
 from random import randint
 from time import time as timer 
  
+#музло
+mixer.init()
+mixer.music.load('muzlo.mp3')
+mixer.music.play()
 
 #картинки
 racket = 'racket.png'
@@ -9,7 +13,7 @@ ten_ball = 'tenis_ball.png'
 #класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
 #конструктор класса
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed,size_x, size_y):
         #вызываем конструктор класса (Sprite):
         sprite.Sprite.__init__(self)
         #каждый спрайт должен хранить свойство image - изображение
@@ -30,13 +34,13 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_width - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
     def update_right(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_width - 80:
+        if keys[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
     
 back = (0,255,255)
@@ -45,43 +49,66 @@ win_height = 500
 window = display.set_mode((win_width,win_height))
 window.fill(back)
 
-#фонты
+#фонты и переменные
+sc1 = 0
+sc2 = 0
 font.init()
 font1 = font.SysFont('Calibri', 50)
 win1 = font1.render('Player 1 win!',True,(255,0,0))
 win2 = font1.render('Player 2 win!',True,(255,0,0))
 
+
+
 #игроки
-player1 = Player(racket,30,200,50,150,4)
-player2 = Player(racket,520,200,50,150,4)
-ball = GameSprite(ten_ball,200,200,50,50,4)
+player1 = Player(racket,30,200,4,50,150)
+player2 = Player(racket,520,200,4,50,150)
+ball = GameSprite(ten_ball,200,200,2,50,50)
 
 run = True
 finish = False
 
-speed_x = 3
-speed_y = 3
+speed_x = 4
+speed_y = 4
 
 FPS = 60
 clock = time.Clock()
 
 while run:
+    last_time = timer()
     #событие нажатия на кнопку “Закрыть”
     for e in event.get():
         if e.type == QUIT:
             run = False
     
+    last_time = timer()
+
     if not finish:
+
+        score_p1 = font1.render(str(sc1),True,(255,255,0))
+        score_p2 = font1.render(str(sc2),True,(255,255,0))
+
+        player1_text = font1.render('1',True,(0,0,255))
+        player2_text = font1.render('2',True,(255,0,0))
+
         window.fill(back)
-        player1.update_left()
-        player2.update_right()
+        player1.update_right()
+        player2.update_left()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
 
-        if sprite.collide_rect(ball,player1) or sprite.collide_rect(ball,player2):
+        window.blit(score_p1,(200,0))
+        window.blit(score_p2,(410,0))
+
+        window.blit(player1_text,(100, 50))
+        window.blit(player2_text,(450, 50))
+
+        if sprite.collide_rect(ball,player1):
             speed_x *= -1
-            speed_y *= -1
-        if ball.rect.y < win_height-50 or ball.rect.y < 0:
+            sc1 += 1
+        if sprite.collide_rect(ball,player2):
+            speed_x *= -1
+            sc2 += 1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
             speed_y *= -1
         
         if ball.rect.x < 0:
@@ -95,6 +122,10 @@ while run:
         player1.reset()
         player2.reset()
         ball.reset()
+        now_time = timer()
+        if now_time - last_time >= 181:
+            mixer.music.play()
 
+    
     display.update()
     clock.tick(FPS)
